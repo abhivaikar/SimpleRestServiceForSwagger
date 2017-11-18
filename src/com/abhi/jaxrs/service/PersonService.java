31a5bb2bd668f01cc03c2d70519f021ca0c82e5b
Abhijeet Vaikar <vaikar.abhijeet@gmail.com>
2017-11-18 19:39:59 +0530

    Working version of simple rest service

---
+++ src/com/abhi/jaxrs/service/PersonService.java
@@ -0,0 +1,87 @@
+package com.abhi.jaxrs.service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.abhi.jaxrs.model.Person;
+import com.abhi.jaxrs.model.Response;
+
+@Path("/person")
+@Consumes(MediaType.APPLICATION_XML)
+@Produces(MediaType.APPLICATION_XML)
+public class PersonService {
+	
+	private static Map<Integer,Person> persons = new HashMap<Integer,Person>();
+
+	@POST
+    @Path("/add")
+	public Response addPerson(Person p) {
+		Response response = new Response();
+		if(persons.get(p.getId()) != null){
+			response.setStatus(false);
+			response.setMessage("Person already exists");
+			return response;
+		}
+		persons.put(p.getId(), p);
+		response.setStatus(true);
+		response.setMessage("Person created successfully");
+		return response;
+	}
+
+	
+	@GET
+    @Path("/{id}/delete")
+	public Response deletePerson(@PathParam("id") int id) {
+		Response response = new Response();
+		if(persons.get(id) == null){
+			response.setStatus(false);
+			response.setMessage("Person doesn't exist");
+			return response;
+		}
+		persons.remove(id);
+		response.setStatus(true);
+		response.setMessage("Person deleted successfully");
+		return response;
+	}
+
+	
+	@GET
+	@Path("/{id}/get")
+	public Person getPerson(@PathParam("id") int id) {
+		return persons.get(id);
+	}
+	
+	@GET
+	@Path("/{id}/getDummy")
+	public Person getDummyPerson(@PathParam("id") int id) {
+		Person p = new Person();
+		p.setAge(99);
+		p.setName("Dummy");
+		p.setId(id);
+		return p;
+	}
+
+	
+	@GET
+	@Path("/getAll")
+	public Person[] getAllPersons() {
+		Set<Integer> ids = persons.keySet();
+		Person[] p = new Person[ids.size()];
+		int i=0;
+		for(Integer id : ids){
+			p[i] = persons.get(id);
+			i++;
+		}
+		return p;
+	}
+
+}
